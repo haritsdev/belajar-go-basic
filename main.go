930f655f65a15b8eaa9a7f6e8c26796d3c09fa82
haritsdev <teguhcodedev1@gmail.com>
2022-05-08 03:43:24 +0700

    PART 3 Tipe Data Bilangan Desimal Golang

--- main.go
+++ main.go
@@ -3,8 +3,15 @@ package main
 import "fmt"
 
 func main(){
+	//* 1. TIPE DATA NUMERIK NON-DESIMAL
 	var positiveNumber uint8= 89
 	var negativeNumber int = -123456789
 	fmt.Printf("bilangan positif: %d\n",positiveNumber)
 	fmt.Printf("bilangan Negative: %d\n",negativeNumber)
+	//*2 TIPE DATA NUMERIK DESIMAL
+	// * ada 2 yait float32 dan float64
+	var decimalNumber=2.62
+	fmt.Printf("bilangan desimal : %f\n",decimalNumber)
+	fmt.Printf("bilangan desimal 3 digit : %.3f\n",decimalNumber)
+	fmt.Printf("bilangan desimal 2 digit: %.2f\n",decimalNumber)
 }
