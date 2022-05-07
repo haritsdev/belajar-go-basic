bb03c6b62eadbe8db26689f223b6d6a880e2b6e1
haritsdev <teguhcodedev1@gmail.com>
2022-05-08 03:50:43 +0700

    PART 3 Tipe data string

--- main.go
+++ main.go
@@ -8,10 +8,25 @@ func main(){
 	var negativeNumber int = -123456789
 	fmt.Printf("bilangan positif: %d\n",positiveNumber)
 	fmt.Printf("bilangan Negative: %d\n",negativeNumber)
+
 	//*2 TIPE DATA NUMERIK DESIMAL
 	// * ada 2 yait float32 dan float64
 	var decimalNumber=2.62
 	fmt.Printf("bilangan desimal : %f\n",decimalNumber)
 	fmt.Printf("bilangan desimal 3 digit : %.3f\n",decimalNumber)
 	fmt.Printf("bilangan desimal 2 digit: %.2f\n",decimalNumber)
+
+	//*3 Tipe Data bool (BOOLEAN)
+	var exist bool =true
+	fmt.Printf("exist? %t\n",exist)
+
+	//*4 Tipe Data String
+	var message string ="Selamat Belajar golang"
+	var biodata=`Nama Saya "TEGUH MUHAMMAD HARITS".
+		     Salam Kenal.
+		     Mari belajar "Golang"
+		     .
+	`
+	fmt.Printf("message: %s \n",message)
+	fmt.Printf("biodata: %s \n",biodata)
 }
