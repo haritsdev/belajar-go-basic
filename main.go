ed38489ff826df2194683e4984a90d6a129cc9da
haritsdev <teguhcodedev1@gmail.com>
2022-05-08 04:36:21 +0700

    PART 5 OPERATOR

--- main.go
+++ main.go
@@ -5,6 +5,26 @@ import (
 )
 
 func main(){
-	const firstName string = "harits"
-	fmt.Print("halo",firstName,"!\n")
+	//* OPERATOR ARITMATIKA
+	var value uint= (((2+6)%3)*4-2)/3
+	fmt.Print("nilai: ",value,"!\n")
+
+	//* OPERATOR PERBANDINGAN
+	var isEqual =(value==2)
+	fmt.Printf("nilai %d (%t) \n",value,isEqual)
+
+	//* Operator Logika
+	//digunakan untuk mencari benar tidaknya kombinasi tipe data bertipe bool
+
+	var left=false
+	var right =true
+	var leftAndRight= left && right
+	fmt.Printf("left && right \t(%t) \n",leftAndRight)
+
+	var leftOrRight = left || right
+	fmt.Printf("left || right \t(%t) \n",leftOrRight)
+
+	var leftReverse= !left
+	fmt.Printf("!left \t\t(%t) \n",leftReverse)
+
 }
---
+++ operator.go
@@ -0,0 +1,6 @@
+
+import "fmt"
+
+func operator() {
+	var value = (((2+6)%3)*4-2)/3
+}
